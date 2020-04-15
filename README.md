3424aa28fd1febacf74ca0663b1efc8c257eb458
Joshua Tenner <tenner.joshua@gmail.com>
2020-04-15 14:10:44 -0400

    prettier

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # assemblyscript-lang README
 
-This is a quick and dirty AssemblyScript language plugin.  It uses a modified verions of the Microsoft TypeScript grammar.  There may be some TypeScript features that are unsupported in AssemblyScript that still remain in the grammar.  There could aslo be some AssemblyScript grammar still missing.  If you see something please conatct me (Rick Battagline) on Twitter: @battagline
+This is a quick and dirty AssemblyScript language plugin. It uses a modified verions of the Microsoft TypeScript grammar. There may be some TypeScript features that are unsupported in AssemblyScript that still remain in the grammar. There could aslo be some AssemblyScript grammar still missing. If you see something please conatct me (Rick Battagline) on Twitter: @battagline
 
 ## Features
 
-I'm attempting to suppor the entire grammar of AssemblyScript (see assemblyscript.org).  This plugin is rather quick and dirty and I plan on improving it over time.  
+I'm attempting to suppor the entire grammar of AssemblyScript (see assemblyscript.org). This plugin is rather quick and dirty and I plan on improving it over time.
 
 ## Known Issues
 
-This is a work in progress.  Please contact me if you see some of the AssemblyScript grammar that is missing, or if there is some of the TypeScript grammar that is unsupported in AssemblyScript.  I chose the extension .asc for the AssemblyScript files.  I did this rather arbitrarily so a different extension may be used in the future.
+This is a work in progress. Please contact me if you see some of the AssemblyScript grammar that is missing, or if there is some of the TypeScript grammar that is unsupported in AssemblyScript. I chose the extension .asc for the AssemblyScript files. I did this rather arbitrarily so a different extension may be used in the future.
 
 ## Release Notes
 
@@ -26,19 +26,19 @@ Fixed issue #.
 
 Added features X, Y, and Z.
 
------------------------------------------------------------------------------------------------------------
+---
 
 ## Working with Markdown
 
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
+**Note:** You can author your README using Visual Studio Code. Here are some useful editor keyboard shortcuts:
 
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
+- Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
+- Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
+- Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
 
 ### For more information
 
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
+- [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
+- [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
 
 **Enjoy!**
--- src/client/index.ts
+++ src/client/index.ts
@@ -1,48 +1,61 @@
-
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as path from "path";
 import { workspace, ExtensionContext } from "vscode";
-import { ServerOptions, TransportKind, LanguageClient, LanguageClientOptions } from "vscode-languageclient";
+import {
+  ServerOptions,
+  TransportKind,
+  LanguageClient,
+  LanguageClientOptions,
+} from "vscode-languageclient";
 // import * as langtypes from "vscode-languageserver-types";
-
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: ExtensionContext) {
-      // The server is implemented in another project and outputted there
-      let serverModule = context.asAbsolutePath(path.join('lib', 'server', 'index.js'));
+  // The server is implemented in another project and outputted there
+  let serverModule = context.asAbsolutePath(
+    path.join("lib", "server", "index.js")
+  );
 
-      // The debug options for the server
-      // TODO: Figure out what this means
-      let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
-      
-      // If the extension is launched in debug mode then the debug server options are used
-      // Otherwise the  normal ones are used
-      let serverOptions: ServerOptions = {
-          run : { module: serverModule, transport: TransportKind.ipc },
-          debug: { module: serverModule, transport: TransportKind.ipc, options: debugOptions }
-      }
-      
-      // Options of the language client
-      let clientOptions: LanguageClientOptions = {
-          // Activate the server for DOT files
-          documentSelector: ['asc'],
-          synchronize: {
-              // Synchronize the section 'assemblyScriptLanguageServer' of the settings to the server
-              configurationSection: 'assemblyScriptLanguageServer',
-              // Notify the server about file changes to '.clientrc files contained in the workspace
-              fileEvents: workspace.createFileSystemWatcher('**/.asc')
-          }
-      }
-      
-      // Create the language client and start the client.
-      let disposable = new LanguageClient('assemblyScriptLanguageServer', 'Language Server', serverOptions, clientOptions).start();
-      
-      // Push the disposable to the context's subscriptions so that the 
-      // client can be deactivated on extension deactivation
-      context.subscriptions.push(disposable);
-  
+  // The debug options for the server
+  // TODO: Figure out what this means
+  let debugOptions = { execArgv: ["--nolazy", "--debug=6009"] };
+
+  // If the extension is launched in debug mode then the debug server options are used
+  // Otherwise the  normal ones are used
+  let serverOptions: ServerOptions = {
+    run: { module: serverModule, transport: TransportKind.ipc },
+    debug: {
+      module: serverModule,
+      transport: TransportKind.ipc,
+      options: debugOptions,
+    },
+  };
+
+  // Options of the language client
+  let clientOptions: LanguageClientOptions = {
+    // Activate the server for DOT files
+    documentSelector: ["asc"],
+    synchronize: {
+      // Synchronize the section 'assemblyScriptLanguageServer' of the settings to the server
+      configurationSection: "assemblyScriptLanguageServer",
+      // Notify the server about file changes to '.clientrc files contained in the workspace
+      fileEvents: workspace.createFileSystemWatcher("**/.asc"),
+    },
+  };
+
+  // Create the language client and start the client.
+  let disposable = new LanguageClient(
+    "assemblyScriptLanguageServer",
+    "Language Server",
+    serverOptions,
+    clientOptions
+  ).start();
+
+  // Push the disposable to the context's subscriptions so that the
+  // client can be deactivated on extension deactivation
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
